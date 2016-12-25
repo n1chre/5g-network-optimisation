7ea0053fe7e7b23be2aa6d7d7a587bd2bb9ce077
Filip Hrenić <hrenic.filip@gmail.com>
2016-12-25 23:23:38 +0100

    main class

--- src/hr/fer/tel/hmo/Main.java
+++ src/hr/fer/tel/hmo/Main.java
@@ -1,21 +1,28 @@
 package hr.fer.tel.hmo;
 
 import hr.fer.tel.hmo.instance.Instance;
+import hr.fer.tel.hmo.network.Network;
+import hr.fer.tel.hmo.solution.Evaluator;
+import hr.fer.tel.hmo.vnf.Component;
+import hr.fer.tel.hmo.vnf.ServiceChain;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
- * Created by fhrenic on 22/12/2016.
  */
 public class Main {
 
 	public static void main(String[] args) {
 
-		args = new String[]{"/Users/fhrenic/Programming/vnf-placer/instance_small.txt"};
+		int x = 0;
+
+		String name = "instance" + (x == 0 ? "_small" : "") + ".txt";
+		args = new String[]{"/Users/fhrenic/Programming/vnf-placer/" + name};
 
 		InputStream stream;
 		if (args.length > 0) {
@@ -23,21 +30,35 @@ public class Main {
 				Path p = Paths.get(args[0]);
 				stream = Files.newInputStream(p);
 			} catch (IOException ex) {
+				System.err.println("File not found, reading from stdin");
 				stream = System.in;
 			}
 		} else {
 			stream = System.in;
 		}
 
-		Instance id;
+		Instance instance;
 		try {
-			id = Instance.readFromStream(stream);
+			instance = Instance.readFromStream(stream);
 		} catch (IOException ex) {
 			System.err.println("Error while reading from stream");
+			ex.printStackTrace();
+			return;
+		} catch (IllegalArgumentException ex) {
+			ex.printStackTrace();
 			return;
 		}
 
-		System.out.println(id.getVnfDemands());
+		System.err.println("Network configured...");
+
+		Network network = instance.getNetwork();
+		Component[] components = instance.getComponents();
+		List<ServiceChain> serviceChains = instance.getServiceChains();
+
+		// Create evalator
+		Evaluator evaluator = new Evaluator(network, components, serviceChains);
+
+		System.err.println("Evaluator created...");
 
 	}
 
